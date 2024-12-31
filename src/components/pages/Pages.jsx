5f379a880aafc31881bbbb29992b7003b676d552
k7aled <khd.rashidi@gmail.com>
2024-12-31 23:53:39 +0330

    add Browser Route to pages

--- src/components/pages/Pages.jsx
+++ src/components/pages/Pages.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Header from "../common/Header/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "../home/Home";
 
 const Pages = () => {
   return (
     <>
       <BrowserRouter>
         <Header />
-        <Routes></Routes>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
